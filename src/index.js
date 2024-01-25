848cc6ceb5923940168afd8b290c88c52292b276
Diwakar Giri <diwakargiri23@gmail.com>
2024-01-25 12:15:13 +0530

    utils code setpup almost done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 // require('dotenv').config({ path: './env' })
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({ path: './env' })
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Error", error);
+            throw error
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("Mongodb connection failed !!", err);
+    })
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+
+// Function handler using promise.
+const asyncHandler = (reqHandler) => {
+    (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
+
+
+/*
+// function handler using try Catch.
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (err) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+*/
